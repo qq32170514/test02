0bc0f9398f751c0f3a36e70c309fa26c767746e4
qq32170514 <qq32170514@gmail.com>
2020-04-14 15:30:07 +0800

    go

--- myscripts.js
+++ myscripts.js
@@ -1,30 +1,3 @@
 
 
 
-var stage = new Konva.Stage({
-    container: 'container',  
-    width: 160,
-    height: 160
-  });
-  
-
-  var layer = new Konva.Layer();
-  
-
-  var circle = new Konva.Circle({
-    x: stage.width() / 2,   
-    y: stage.height() / 2,  
-    radius: 70,             
-    fill: 'red',            
-    stroke: 'black',        
-    strokeWidth: 4          
-  });
-  
- 
-  layer.add(circle);
-  
-  
-  stage.add(layer);
-  
- 
-  layer.draw();
