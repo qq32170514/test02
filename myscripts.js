2a2c589d03048a4853b1b8ba44da28709bab5478
qq32170514 <qq32170514@gmail.com>
2020-04-14 15:58:56 +0800

    hh

--- myscripts.js
+++ myscripts.js
@@ -8,27 +8,19 @@ var stage = new Konva.Stage({
   
   // then create a layer
   var layer = new Konva.Layer();
-  var layer = new damn.Layer();
+
   
   // create our shape
   var circle = new Konva.Circle({
-    x: stage.width() / 2,   // x position of the center
-    y: stage.height() / 2,  // y position of the center
-    radius: 70,             // circle radius
-    fill: 'red',            // color to fill the area
-    stroke: 'black',        // color to draw the line
+    x: stage.width() / 5,   // x position of the center
+    y: stage.height() / 5,  // y position of the center
+    radius: 180,             // circle radius
+    fill: 'green',            // color to fill the area
+    stroke: 'red',        // color to draw the line
     strokeWidth: 4          // line width
   });
 
-  var circle = new damn.Circle({
-    x: stage.width() / 2,   // x position of the center
-    y: stage.height() / 2,  // y position of the center
-    radius: 300,             // circle radius
-    fill: 'green',            // color to fill the area
-    stroke: 'yellow',        // color to draw the line
-    strokeWidth: 20          // line width
-  });
-  
+
   // add the shape to the layer
   layer.add(circle);
   
