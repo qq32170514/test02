afb07b73670eb924d957f6359c69d9ddadfe1f83
qq32170514 <qq32170514@gmail.com>
2020-04-14 16:07:08 +0800

    mm

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,8 @@
 // first we need to create a stage
 var stage = new Konva.Stage({
     container: 'container',   // id of container <div>
-    width: 320,
-    height: 320
+    width: 160,
+    height: 160
   });
   
   // then create a layer
@@ -12,10 +12,10 @@ var stage = new Konva.Stage({
   var circle = new Konva.Circle({
     x: stage.width() / 2,   // x position of the center
     y: stage.height() / 2,  // y position of the center
-    radius: 140,             // circle radius
+    radius: 70,             // circle radius
     fill: 'red',            // color to fill the area
     stroke: 'black',        // color to draw the line
-    strokeWidth: 10          // line width
+    strokeWidth: 4          // line width
   });
   
   // add the shape to the layer
